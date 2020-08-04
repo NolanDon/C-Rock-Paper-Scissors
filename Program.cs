e8aecf8e17cc42ab42420f6057958fe503e42ce3
Nolan <nolan.boxill@neofinancial.com>
2020-08-03 22:58:13 -0600

    add if else statements to determine winner

--- Program.cs
+++ Program.cs
@@ -19,12 +19,47 @@ namespace csharp
             switch (randomInt) {
                 case 1: 
                 inputCPU = "rock";
+                Console.WriteLine("computer chose rock");
+                if (inputPlayer == "rock") {
+                    Console.WriteLine("Draw"); 
+                }
+                else if (inputPlayer == "paper") {
+                    Console.WriteLine("player wins");
+                    scorePlayer++;
+                }
+                else if (inputPlayer == "scissors") {
+                    Console.WriteLine("computer wins");
+                    scoreCPU++;
+                }
                 break;
                 case 2: 
                 inputCPU = "paper";
+                if (inputPlayer == "paper") {
+                    Console.WriteLine("Draw"); 
+                }
+                else if (inputPlayer == "rock") {
+                    Console.WriteLine("player wins");
+                    scorePlayer++;           
+                }
+                else if (inputPlayer == "rock") {
+                    Console.WriteLine("computer wins");
+                    scoreCPU++;
+
+                }
                 break;
                 case 3: 
                 inputCPU = "scissors";
+                if (inputPlayer == "scissors") {
+                    Console.WriteLine("Draw"); 
+                }
+                else if (inputPlayer == "paper") {
+                    Console.WriteLine("computer wins");
+                    scoreCPU++;
+                }
+                else if (inputPlayer == "rock") {
+                    Console.WriteLine("player wins");
+                    scorePlayer++
+                }
                 break;
                 default:
                 break;
