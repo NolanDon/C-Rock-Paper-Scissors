b8a3a62fae39b4cbf1e33f2a80ed1cccf48ca18a
Nolan <nolan.boxill@neofinancial.com>
2020-08-03 22:52:31 -0600

    basic Input logic for options

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputPlayer, inputCPU;
+            int randomInt;
+
+            Console.Write("choose between rock, paper and scissors");
+            inputPlayer = Console.ReadLine();
+
+            Random rnd = new Random();
+
+            randomInt = rnd.Next(1,4);
+
+            switch (randomInt) {
+                case 1: 
+                inputCPU = "rock";
+                break;
+                case 2: 
+                inputCPU = "paper";
+                break;
+                case 3: 
+                inputCPU = "scissors";
+                break;
+                default:
+                break;
+            }
         }
     }
 }
